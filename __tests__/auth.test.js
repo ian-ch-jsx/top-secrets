6bd9d958412396208f218134f573b258dd62b60c
Ian Christiansen <ianc.songdog@gmail.com>
2022-03-29 14:05:14 -0700

    test written for login functionality

--- __tests__/auth.test.js
+++ __tests__/auth.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
 
 describe('top-secrets routes', () => {
   beforeEach(() => {
@@ -18,11 +19,25 @@ describe('top-secrets routes', () => {
   };
 
   it('signs up a user via POST', async () => {
-    const res = await request(app).post('/api/v1/auth/').send(mockUser);
+    const res = await request(app).post('/api/v1/auth/register').send(mockUser);
 
     expect(res.body).toEqual({
       id: expect.any(String),
       email: 'test@example.com',
+    });
+  });
+
+  it('signs in an existing user', async () => {
+    const user = await UserService.create({
+      email: 'test@example.com',
+      password: 'password',
+    });
+
+    const res = await request(app).post('/api/v1/auth/login').send(mockUser);
+
+    expect(res.body).toEqual({
+      message: 'Login successful',
+      user,
     });
   });
 });
